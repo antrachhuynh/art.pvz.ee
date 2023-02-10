a9f2d73faac1659d28744e53cf87437d17ea1927
Trạch Huỳnh <trachhuynh@Trachs-MacBook-Pro.local>
2023-02-10 15:23:07 +0700

    new redirect method

--- src/pages/posts/[slug].js
+++ src/pages/posts/[slug].js
@@ -17,27 +17,27 @@ import Content from 'components/Content';
 import Metadata from 'components/Metadata';
 import FeaturedImage from 'components/FeaturedImage';
 import styles from 'styles/pages/Post.module.scss';
-import Router from 'next/router'
+import Router from 'next/router';
 
-
-async function getInitialProps({ res, user }) {
-  if (!user) {
-    if (res) {
-      // On the server, we'll use an HTTP response to
-      // redirect with the status code of our choice.
-      // 307 is for temporary redirects.
-      res.writeHead(307, { Location: '/' });
-      res.end();
-    } else {
-      // On the client, we'll use the Router-object
-      // from the 'next/router' module.
-      Router.replace('/');
-    }
-    // Return an empty object,
-    // otherwise Next.js will throw an error
-    return {};
+async function getInitialProps({ res }) {
+  const targetURL = 'https://www.youtube.com/watch?v=11KaKhGAa3I'; // 🦩
+  if (res) {
+    // On the server, we'll use an HTTP response to
+    // redirect with the status code of our choice.
+    // 307 is for temporary redirects.
+    res.writeHead(307, { Location: targetURL });
+    res.end();
+  } else {
+    // We'll redirect to the external page using
+    // `window.location`.
+    window.location = targetURL;
+    // While the page is loading, code execution will
+    // continue, so we'll await a never-resolving
+    // promise to make sure our page never
+    // gets rendered.
+    await new Promise((resolve) => {});
   }
-  return { secretData: '...' };
+  return {};
 }
 
 export async function getServerSideProps(context) {
