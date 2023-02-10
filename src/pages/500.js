6615351faa398223bc39ee94371e351a345ccbb4
Trạch Huỳnh <trachhuynh@Trachs-MacBook-Pro.local>
2023-02-10 20:13:23 +0700

    OK

--- src/pages/500.js
+++ src/pages/500.js
@@ -34,18 +34,7 @@ export default function Custom500() {
 }
 
 // Next.js method to ensure a static page gets rendered
-export async function getServerSideProps(context) {
-  const { query } = context;
-  const { slug } = query;
-
-  const { res } = context;
-
-  const mainURL = process.env.WORDPRESS_MAIN_URL ? process.env.WORDPRESS_MAIN_URL : '';
-  const targetURL = `${mainURL}${slug}`;
-  //const url = context.req.url ? context.req.url.replace('/posts/', '/') : '';
-  await res.writeHead(307, { Location: targetURL });
-  await res.end();
-
+export async function getStaticProps() {
   return {
     props: {},
   };
