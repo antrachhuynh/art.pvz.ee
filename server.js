0f232c75daaa52c7d38742efef0b9772563b2e25
Trạch Huỳnh <trachhuynh@Trachs-MacBook-Pro.local>
2023-02-11 13:19:29 +0700

    Redirect from server-side 4

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
 const express = require('express');
 const next = require('next');
 
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
+const app = next({ dev: process.env.NODE_ENV !== 'production' });
 const handle = app.getRequestHandler();
+const mainURL = process.env.WORDPRESS_MAIN_URL ? process.env.WORDPRESS_MAIN_URL : '';
 
 app.prepare().then(() => {
   const server = express();
 
   server.get('*', (req, res) => {
-    const { pathname, query } = req;
-
-    if (pathname === '/_error') {
-      const { statusCode, url } = query;
-      if (statusCode === '500' || statusCode === '503') {
-        res.redirect(`https://your-wordpress-site.com/${url}`);
-      }
-    }
-
     return handle(req, res);
+  });
+
+  server.use((err, req, res) => {
+    if (err.statusCode === 500 || err.statusCode === 503 || err.statusCode === 403 || err.statusCode === 404) {
+      console.log('redirecting...');
+      res.redirect(307, `${mainURL}${req.url}`);
+    }
   });
 
   server.listen(3000, (err) => {
